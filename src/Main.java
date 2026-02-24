f318d84718853b89ed06e05f3ef42e94825ef55a
Cleber <cleber_august@yahoo.com.br>
2026-02-24 08:17:08 -0300

    feat: implementa busca de CEP com integração ViaCEP e geração de JSON

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner reading = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+
+        System.out.println("Digite um numero de CEP para consulta: ");
+
+        var cep = reading.nextLine();
+
+        try{
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+
+
+
+
+
+
+
+
+    }
+}
